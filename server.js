d5fc76cf1b1efa69856aadcf78231e6ab7d84ae3
Aya berjawi <ayabarjawi@gmail.com>
2021-03-01 14:03:37 +0200

    lab11

--- server.js
+++ server.js
@@ -3,72 +3,64 @@
 const express = require('express');
 require('dotenv').config();
 
-
-const PORT = process.env.PORT || 3000;
-const server = express();
+const cors = require('cors');
 const superagent = require('superagent');
-server.use(express.urlencoded({ extended: true }));
 
-
+const PORT = process.env.PORT || 3000; 
+const server = express();
+server.use(cors()); 
 server.set('view engine', 'ejs');
 
-
 server.use(express.static('./public'));
+server.use(express.urlencoded({extended:true}));
+
+server.get('/hello', (req,res) => {
+    res.render('./pages/index');
+})
+
+server.get('/searches/new', (req,res) => {
+    res.render('./searches/new');
+})
+
+server.post('/searches', (req,res)=>{
+    console.log('Hello from search');
+    let searchInput = req.body.search;
+    let key= process.env.GOOGLE_API_KEY;
+    let url;
+    if (req.body.searchValue === 'title'){
+        url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}+intitle`;
+    }else{
+        url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}+inauthor`;
+    }
+    console.log('Hello from search22');
+   superagent.get(url)
+    .then(result =>{
+        console.log(result);
+        let booksArray = result.body.items.map((item)=>{
+        return new Book(item);
+    })
+    
+    res.render('searches/show', { books: booksArray });
+    })
+    .catch(()=>{
+                errorHandler('Error in getting data from BooksAPI');
+            })
+
+})
+
+
 
 server.get('/', (req,res) => {
     res.render('./pages/index');
 })
 
-
-server.get('/hello',(req,res) =>{
-    res.render('./pages/index')
+server.get('/error', (req,res) => {
+    errorHandler('Error!!');
 })
 
-
-server.get('/new', (req,res) => {
-    res.render('/pages/searches/new');
+server.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
 })
-
-server.post('/new', (req,res) =>{
-    let search= req.query.search;
-    // let title = req.query.title;
-    // let author = req.query.author;
-    let key = process.env.GOOGLE_API_KEY;
-    let url;
-    if ($('#title').is(":checked"))
-    {
-    url = `https://www.googleapis.com/books/v1/volumes?q=${search}+intitle:keyes&key=${key}`
-    }else{
-        url =`https://www.googleapis.com/books/v1/volumes?q=${search}+inauthor:keyes&key=${key}`;
-    }
-    superagent.get(url)
-    .then(result => {
-        let booksArray = result.body.items.volumeInfo.map((item)=>{
-            return new Book(item);
-    })
-    $('button').click(()=>{
-
-        res.render('./pages/searches/show',{bookData:booksArray});
-    })
-    })
-    .catch(()=>{
-        errorHandler('Error in getting data from BooksAPI');
-    })
-});
-
-// server.post('/searches',(req,res) =>{
-//     res.render('./pages/new')
-// })
-
-function Book(book) {
-    this.title = book.title;
-    this.url = book.imageLinks.smallThumbnail;
-    this.authors = book.authors[0];
-    this.description = book.description;
-
-}
-
-
 
 function errorHandler(errors) {
     server.use('*',(req,res)=>{
@@ -76,6 +68,12 @@ function errorHandler(errors) {
     })
 }
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-})
+
+function Book(data) {
+    this.image_url =
+      (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) ||
+      "https://i.imgur.com/J5LVHEL.jpg";
+    this.title = data.volumeInfo.title;
+    this.author = data.volumeInfo.authors;
+    this.description = data.volumeInfo.description || "There is no description";
+  }
